49c44d958cee9d9cd190399cb92e108d5f0fee5c
Jonathan <jondmanning1986@gmail.com>
2024-10-25 10:49:59 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import NoMatch from "./NoMatch";
+import UserTodosView from "./UserTodosView";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  // Load Users
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function loadUsers() {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users`,
+          { signal: abortController.signal }
+        );
+
+        const users = await response.json();
+        setUsers(users);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
+      }
+    }
+
+    loadUsers();
+
+    return () => {
+      abortController.abort(); // Cancels any pending request or response
+    };
+  }, []);
+
+  // Create Users list
+  const listItems = users.map((user) => (
+    <li className="nav-item" key={user.id}>
+      <Link className="nav-link link-primary" to={`/users/${user.id}/todos`} onClick={() => console.log(user)}>
+        {user.name}
+      </Link>
+    </li>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <main className="container">
+        <div className="row">
+          <aside className="border border-secondary-subtle col-xs-6 col-md-3">
+            <h2 className="my-4 text-center">Users</h2>
+            <ul className="nav flex-column text-center mb-3">{listItems}</ul>
+          </aside>
+          <section className="col-xs-6 col-md-9">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/users/:id/todos" element={<UserTodosView />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/UserTodosView.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function UserTodosView() {
+  const [todos, setTodos] = useState([]);
+
+    const params = useParams();
+    const id = params.id;
+
+    useEffect(() => {
+        console.log(id);
+    }, [id]);
+
+  // Load Users
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function loadTodosForUser(id) {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}/todos`,
+          { signal: abortController.signal }
+        );
+
+        const todos = await response.json();
+        setTodos(todos);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
+      }
+    }
+
+    loadTodosForUser(id);
+
+    return () => {
+      abortController.abort(); // Cancels any pending request or response
+    };
+  }, [id]);
+
+  const listItems = todos.map((todo) => (
+    <li
+      key={todo.id}
+      style={{ textDecoration: todo.completed ? "line-through" : "" }}
+    >
+      {todo.title}
+    </li>
+  ));
+
+  return (
+    <>
+      <h3 className="m-2">Todo items</h3>
+      <ul>{listItems}</ul>
+    </>
+  );
+}
+
+export default UserTodosView;
